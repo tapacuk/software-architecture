493e0a0e5736152119064d44eb01bc7b5d203130
tapacuk <announcerf1@gmail.com>
2026-02-16 20:43:23 +0200

    Delete src directory

--- src/entities/phone.ts
+++
@@ -1,8 +0,0 @@
-import { BaseDevice } from './base-device';
-import type { IBattery } from './interfaces/battery.interface';
-
-export class Phone extends BaseDevice {
-  constructor(brand: string, battery: IBattery) {
-    super(brand, false, true, false, true, battery);
-  }
-}
