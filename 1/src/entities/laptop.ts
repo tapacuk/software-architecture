a54165de5ed50f71fbbc7dfd2f83861826922800
tapacuk <announcerf1@gmail.com>
2026-02-16 20:41:27 +0200

    Delete 1/src directory

--- 1/src/entities/laptop.ts
+++
@@ -1,8 +0,0 @@
-import { BaseDevice } from './base-device';
-import type { IBattery } from './interfaces/battery.interface';
-
-export class Laptop extends BaseDevice {
-  constructor(brand: string, battery: IBattery) {
-    super(brand, false, false, false, true, battery);
-  }
-}
